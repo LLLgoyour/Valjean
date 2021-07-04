6cc422552536743aba508c8846c53bd3906f533a
Shinonome Masaki <lllgoyour@outlook.com>
2021-07-03 22:15:48 -0400

    Update language.php

--- lib/language.php
+++ lib/language.php
@@ -1,7 +1,8 @@
 <?php
-<<<<<<< Updated upstream
 /**
- * Valjean Language File
+ * Valjean Language Setting
+ * Including languages of Zh-CN, Zh-TW, En-US.
+ * 
  * Author: LLLgoyour. All rights reserved.
  */
 
@@ -10,27 +11,14 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 class Valjean_Lang {
     /*
      * 设置面板语言配置列表 Language list in dashboard
-     * 
-     * 
      */
 
 public static function getList() {
     //获取所有语言配置 get all language settings
     $langList = glob(Helper::options()->themeFile(Valjean_Lib::getTheme()))
-}
-=======
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-
-/**
- * Valjean Language Setting
- * Including languages of Zh-CN, Zh-TW, En-US.
- * 
- * Author: LLLgoyour. All rights reserved.
- */
 
 class Language {
     public static function getLang() {
         require_once("")
     }
->>>>>>> Stashed changes
 }
