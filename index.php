5a0f591d67ec37a117f275830d8bf1b2770e0acb
Shinonome Masaki <lllgoyour@outlook.com>
2021-07-21 17:14:19 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Valjean
+ * 深渊尽头，时间原点。 Go approaching the odd under abyss, and observing the origin of time.
+ * 作者：<LLLgoyour href="https://lllgoyour.tk">LLLgoyour</a> | 帮助文档：<a href="https://valjean.lllgoyour.tk>Wiki</a>
+ * 
+ * @package     Valjean
+ * @author      LLLgoyour
+ * @version     1.0
+ * @link        https://lllgoyour.tk
+ */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('lib/header.php');
+$this->
+?>
+    <main class="main-container container" role="main">
+        <div class="post-list">
+            <!-- 文章循环 -->
+            <?php while($this->next()): ?>
+            <br />
+                <!-- 文章模块 -->
+                <div class="post-item" role="article">
+                    <div class=""
+
