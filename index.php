bd24309fa08c1671d68e05ec0a4ccff115e2895e
Shinonome Masaki <lllgoyour@outlook.com>
2021-07-23 14:31:17 -0400

    update

--- index.php
+++ index.php
@@ -12,14 +12,38 @@
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('lib/header.php');
-$this->
+$this->need('lib/')
+
+if (@$_SERVER('HTTP_X_PJAX') == true) {
+    header('HTTP/1.1 200 OK');
+    echo '<title>' . Valjean_Header::title($this, '', ' - ', true, false) . '</title>';
+    echo '<div class=""'
+}
+
 ?>
+
     <main class="main-container container" role="main">
         <div class="post-list">
-            <!-- 文章循环 -->
+            <!-- 文章循环开始 -->
             <?php while($this->next()): ?>
             <br />
                 <!-- 文章模块 -->
                 <div class="post-item" role="article">
-                    <div class=""
-
+                    <div class="container-fluid"><div class="row">
+                <div class="col-md-6 post-banner-box">
+                    <a href="<?php $this->permalink(); ?>" class="post-link">
+                        <div class="post-banner">
+                            <img src="<?php echo> Utils::addLoadingImages($this->options->CDN, $this->options->loading_image,'normal'); ?>" data-gisrc="<?php Utils::postBanner($this); ?>">
+                        </div>
+                    </a>
+                </div>
+                <div class=""
+            <br />
+            <!-- 文章分页 -->
+            <div class="post-pagenavigator">
+                <span class="post-pagenavigator-left"><?php $this->pageLink('<i class="iconfont icon-chevron-left"></i>'); ?></span>
+                <span class="post-pagenavigator-right"><?php $this->pageLink('<i class="iconfont icon-chevron-right"></i>', 'next'); ?></span>    
+            </div>
+        <!-- 文章循环结束 --></div>
+    </main>
+<?php $this->need('lib/footer.php'); ?>
--- lib/themeConfig.php
+++ lib/themeConfig.php
@@ -7,6 +7,12 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 //引用设置组件 import setting plugins
+Valjean::requireFile(__DIR__ . '/initLib/', 'php');
+
+function themeConfig($form) {
+    themeBackUp();
+    $
+}
 
 //语言 language
 $GLOBALS['valjeanLang'] = 'zh-cn';
@@ -15,8 +21,11 @@ $GLOBALS['valjeanLang'] = 'zh-cn';
 //主题CDN字体 theme CDN font
 $GLOBALS['valjeanFontCDN'] = array(
     'if' => 'on', //选择是否开启字体CDN（思源宋体或黑体）
-    'cdn' => 'google_font' //默认选用Google Font。可直接输入font CDN url。
+    'cdn' => 'google_font' //默认选用Google Font。可直接输入字体的CDN url。
 );
 
 //文章缩略图
 $GLOBALS['valjean']
+
+//是否压缩 HTML 源代码
+$GLOBALS['valjeanIfCompressHTML'] = 'off';
